67e18da45684c1a6847207bedcd388d5e4939405
losiik <49565548+losiik@users.noreply.github.com>
2025-06-28 10:57:35 +0300

    move tests to pytest

--- tests/test_klosep_berezovskaia.py
+++ tests/test_klosep_berezovskaia.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,25 +10,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class TestBerezovskaia(unittest.TestCase):
-    def setUp(self) -> None:
-        chrome_options = ChromeOptions()
+BASE_URL = "http://localhost:8000/"
 
-        chrome_options.add_argument("--window-size=1920,1080")
-        # chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-infobars")
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
 
-        service = ChromeService(executable_path=ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
-        # self.driver.implicitly_wait(60)
+@pytest.fixture
+def driver(request):
+    chrome_options = ChromeOptions()
 
-    def tearDown(self) -> None:
-        self.driver.quit()
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
+    request.cls.driver = driver
+    yield
+    driver.quit()
+
+
+@pytest.mark.usefixtures("driver")
+class TestBerezovskaia:
     def find_element(self, path: str) -> WebElement:
-        entity = WebDriverWait(self.driver, 60).until(
+        entity = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(
                 (
                     By.XPATH,
@@ -65,7 +70,7 @@ class TestBerezovskaia(unittest.TestCase):
 
     def get_send_button(self) -> WebElement | None:
         try:
-            send_button = self.find_element( '//*[@id="root"]/div/div/div[2]/button/span')
+            send_button = self.find_element('//*[@id="root"]/div/div/div[2]/button/span')
             return send_button
         except:
             return None
@@ -78,47 +83,47 @@ class TestBerezovskaia(unittest.TestCase):
             return None
 
     def test_card_number_length(self):
-        self.driver.get(url='http://localhost:8000/?balance=33000&reserved=2000')
+        self.driver.get(url=f'{BASE_URL}?balance=33000&reserved=2000')
         self.enable_rubles()
         value = self.card_input("12345678901234567")
-        self.assertLessEqual(len(value), 16, "Card number accepts more then 16 digits")
+        assert len(value) == 16
 
     def test_check_negative_amount(self):
-        self.driver.get(url='http://localhost:8000/?balance=33000&reserved=2000')
+        self.driver.get(url=f'{BASE_URL}?balance=33000&reserved=2000')
         self.enable_rubles()
         self.card_input("1111111111111111")
         self.amount_input("-100")
         send_button = self.get_send_button()
         exception_message = self.get_exception_message()
-        self.assertIsNone(send_button, "The send button should not exist")
-        self.assertIsNotNone(exception_message, "An error about an invalid transaction should be displayed")
+        assert send_button is None
+        assert exception_message is not None
 
     def test_zero_amount(self):
-        self.driver.get(url='http://localhost:8000/?balance=33000&reserved=2000')
+        self.driver.get(url=f'{BASE_URL}?balance=33000&reserved=2000')
         self.enable_rubles()
         self.card_input("1111111111111111")
         self.amount_input("0")
         send_button = self.get_send_button()
         exception_message = self.get_exception_message()
-        self.assertIsNone(send_button, "The send button should not exist")
-        self.assertIsNotNone(exception_message, "An error about an invalid transaction should be displayed")
+        assert send_button is None
+        assert exception_message is not None
 
     def test_dollar_transaction_amount_more_than_the_amount_on_the_account(self):
-        self.driver.get(url='http://localhost:8000/?balance=33000&reserved=2000')
+        self.driver.get(url=f'{BASE_URL}?balance=33000&reserved=2000')
         self.enable_dollars()
         self.card_input("1111111111111111")
         self.amount_input("9000")
         send_button = self.get_send_button()
         exception_message = self.get_exception_message()
-        self.assertIsNone(send_button, "The send button should not exist")
-        self.assertIsNotNone(exception_message, "An error about an invalid transaction should be displayed")
+        assert send_button is None
+        assert exception_message is not None
 
     def test_evro_transaction_amount_more_than_the_amount_on_the_account(self):
-        self.driver.get(url='http://localhost:8000/?balance=33000&reserved=2000')
+        self.driver.get(url=f'{BASE_URL}**?balance=33000&reserved=2000')
         self.enable_evro()
         self.card_input("1111111111111111")
         self.amount_input("5000")
         send_button = self.get_send_button()
         exception_message = self.get_exception_message()
-        self.assertIsNone(send_button, "The send button should not exist")
-        self.assertIsNotNone(exception_message, "An error about an invalid transaction should be displayed")
+        assert send_button is None
+        assert exception_message is not None
